980bee8b4d8002d145a750aad4197dfa0476db98
hejuan <hejuan@sina.com>
2019-07-05 16:02:48 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data(){
+      return{
+        
+      }
+
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+ 
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,28 @@
+import MSite from "../pages/MSite/MSite.vue"
+import Order from "../pages/Order/Order.vue"
+import Search from "../pages/Search/Search.vue"
+import Profile from "../pages/Profile/Profile.vue"
+
+export default[
+    {
+        path:'/msite',
+        component:MSite
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path:"/",
+      redirect: '/msite'
+    }
+
+]
